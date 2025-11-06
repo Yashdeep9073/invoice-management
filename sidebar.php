72f34d161eebdf9c4c3500a0733d9bc9981e1941
Yashdeep <166975335+Yashdeep9073@users.noreply.github.com>
2025-11-06 18:18:13 +0530

    name changed

--- sidebar.php
+++ sidebar.php
@@ -92,7 +92,7 @@ $isAdmin = strtolower($roleData['0']['role_name']) === 'admin';
                                         stroke-linecap="round" stroke-linejoin="round" class="feather feather-smartphone">
                                         <rect x="5" y="2" width="14" height="20" rx="2" ry="2"></rect>
                                         <line x1="12" y1="18" x2="12.01" y2="18"></line>
-                                    </svg><span>Application</span><span class="menu-arrow"></span></a>
+                                    </svg><span>Digital Sheet</span><span class="menu-arrow"></span></a>
                             <?php endif; ?>
                             <ul style="display: none;">
                                 <?php if ($isAdmin || hasPermission('Spread Sheet', $privileges, $roleData['0']['role_name'])): ?>
